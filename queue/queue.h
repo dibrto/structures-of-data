a2c56a8b96fe4b6e1f6f31ea6818a0d169ea815e
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-02 12:18:39 +0200

    Add desctructor and copy constructor

--- queue/queue.h
+++ queue/queue.h
@@ -22,3 +22,22 @@ private:
 template <typename T>
 Queue<T>::Queue(int capacity)
 : size(0), frontElem(0), capacity(capacity), arr(new T[capacity]) {}
+
+template <typename T>
+Queue<T>::~Queue(){
+    delete[] arr;
+}
+
+template <typename T>
+Queue<T>::Queue(const Queue &other){
+    arr =  new T[other.capacity];
+
+    capacity = other.capacity;
+    frontElem = other.frontElem;
+    size = other.size;
+
+    for (size_t i = 0; i < other.size; i++){
+        arr[i] = other.arr[i];
+    }    
+}
+
