b39c5cf0be4c774b7272165690e62fc8eea9b1be
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-02 15:00:29 +0200

    Implement functions file for use every structure

---
+++ functions.cpp
@@ -0,0 +1,25 @@
+#include "functions.h"
+
+void QueueUsage(){
+    Queue<int> q1(100);
+    
+    q1.push(10);
+    q1.push(20);
+    q1.push(30);
+    
+    cout << q1.front() << endl;
+    q1.pop();
+    cout << q1.front() << endl;
+
+    Queue<int> q2(q1);
+    cout << q2.getSize() << endl;
+    q2.print();
+
+    Queue<int> q3;
+    q1 = q1;
+    q3 = q1;
+    q1.pop();
+
+    cout << q3.getSize() << endl;
+    q3.print();
+}
--- main.cpp
+++ main.cpp
@@ -1,25 +1,6 @@
-#include "queue.h"
+#include "functions.h"
 
 int main(int argc, char const *argv[]){
-    Queue<int> q1(100);
-    
-    q1.push(10);
-    q1.push(20);
-    q1.push(30);
-    
-    cout << q1.front() << endl;
-    q1.pop();
-    cout << q1.front() << endl;
-
-    Queue<int> q2(q1);
-    cout << q2.getSize() << endl;
-    q2.print();
-
-    Queue<int> q3;
-    q1 = q1;
-    q1.pop();
-
-    cout << q3.getSize() << endl;
-    q3.print();
+    // QueueUsage();
     return 0;
 }
