f14b7bfd96b41285d3d9d518bf74d28391a40dd7
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-02 15:40:54 +0200

    Implement Deque constructor

--- src/deque.h
+++ src/deque.h
@@ -4,19 +4,27 @@ template <typename T>
 class Deque {
 public:
     Deque(int capacity = 100);
-    ~Deque();
-    Deque(const Deque &other);
+    // ~Deque();
+    // Deque(const Deque &other);
 
-    void insertFront(T elem);
-    void insertBack(T elem);
-    void eraseFront();
-    void eraseBack();
-    T front();
-    T back();
-    int size();
-    bool empty();
+    // void insertFront(T elem);
+    // void insertBack(T elem);
+    // void eraseFront();
+    // void eraseBack();
+    // T front();
+    // T back();
+    // int getSize();
+    // bool empty();
 
 private:
     int size, frontElem, backElem, capacity;
     T* arr;
 };
+
+template <typename T>
+Deque<T>::Deque(int capacity)
+: size(0)
+, frontElem(0)
+, backElem(0)
+, capacity(capacity)
+, arr(new T[capacity]) {}
