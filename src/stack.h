6a09342c1ca1c3d9d59a8769e73d0a972ab4c2a1
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-11-04 23:39:46 +0200

    Implement desctructor, copy and operator =

--- src/stack.h
+++ src/stack.h
@@ -4,9 +4,9 @@ template <typename T>
 class Stack{
 public:
     Stack(int capacity = 100);
-    // ~Stack();
-    // Stack(const Stack<T> &other);
-    // Stack<T> operator=(const Stack<T>& other);
+    ~Stack();
+    Stack(const Stack<T> &other);
+    Stack<T> operator=(const Stack<T>& other);
 
     // int getSize() const;
     // bool isEmpty() const;
@@ -22,7 +22,44 @@ private:
 
 template <typename T>
 Stack<T>::Stack(int capacity)
-: capacity(capacity)
-, size(0)
-, frontIndex(-1)
-, arr(new T[capacity]){}
+    : capacity(capacity)
+    , size(0)
+    , frontIndex(-1)
+    , arr(new T[capacity]){}
+
+template <typename T>
+Stack<T>::~Stack(){
+    delete[] arr;
+}
+
+template <typename T>
+Stack<T>::Stack(const Stack<T> &other){
+    arr = new T[other.capacity];
+
+    capacity = other.capacity;
+    frontIndex = other.frontIndex;
+    size = other.size;
+
+    for (size_t i = 0; i < other.size; i++){
+        arr[i] = other.arr[i];
+    }
+}
+
+template <typename T>
+Stack<T> Stack<T>::operator=(const Stack<T> &other){
+    if (this == &other){
+        cout << "Can not copy to same reference" << endl;
+        return Stack<T>();
+    }
+
+    delete[] arr;
+    capacity = other.capacity;
+    size = other.size;
+    frontIndex = other.frontIndex;
+
+    arr = new T[capacity];
+    for (int i = 0; i < size; ++i)
+        arr[i] = other.arr[i];
+
+    return *this;
+}
